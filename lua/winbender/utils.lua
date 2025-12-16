888149ad635384eda17f14cb96f2eda3fb52a522
Larson, Eric <numeric.larson@gmail.com>
2025-12-16 00:32:35 -0800

    minor

--- lua/winbender/utils.lua
+++ lua/winbender/utils.lua
@@ -6,7 +6,7 @@ function M.prepend_label(title, label)
     if type(title) == "table" then
         local new_title = vim.deepcopy(title)
         table.insert(new_title, 1, {' '})
-        table.insert(new_title, 1, {label,options.quick_access_hl})
+        table.insert(new_title, 1, {label, options.quick_access_hl})
         return new_title
     elseif type(title) == "string" then
         return { {label, options.quick_access_hl}, {' ' .. title} }
